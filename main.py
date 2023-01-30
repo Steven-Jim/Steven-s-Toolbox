ce709500e24cea26e9a2a92d78be9011586ac293
StupidOrNot <don_tforgetpass@126.com>
2023-01-30 19:56:11 +0800

    fix bug when input incorrect

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from modules import Battle_Ship_No_GUI, password_maker, word_backward
 
 
 def check():
+    tool = input('please choose tool')
     if tool == 'word_backward':
         word_backward.main(input('word?'))
     elif tool == 'password_maker':
@@ -18,5 +19,4 @@ def check():
 
 tools = ['word_backward', 'password_maker', 'Battle_Ship_No_GUI']
 print('Welcome to Steven\'s Toolbox! We now have ' + str(len(tools)) + ' tools. They are: ' + str(tools))
-tool = input('please choose tool')
 check()
