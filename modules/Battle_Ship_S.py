9281ad6bc94bf823e138cf90d2cae66bbf4003ae
StupidOrNot <don_tforgetpass@126.com>
2023-01-31 13:06:09 +0800

    change name

---
+++ modules/Battle_Ship_S.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+def main():
+    pass
+
+
+def start():
+    line1 = ['■', '■', '■', '■', '■']
+    line2 = ['■', '■', '■', '■', '■']
+    line3 = ['■', '■', '■', '■', '■']
+    line4 = ['■', '■', '■', '■', '■']
+    line5 = ['■', '■', '■', '■', '■']
+def hit_action(line_number,number_in_line):
+    if line_number==1:
+
